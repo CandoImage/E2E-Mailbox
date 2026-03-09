327d5a911b5a76eeb01c8d5cbc7f157996d628c7
Peter Philipp <peter.philipp@cando-image.com>
2026-03-09 12:17:34 +0100

    Update developermail.test.ts
    Skip developermail tests.

--- tests/developermail.test.ts
+++ tests/developermail.test.ts
@@ -2,6 +2,7 @@ import IntegrationMailbox from '../src/index';
 import { EmailResponse } from '../src/types';
 
 describe("when using DeveloperMail", () => {
+    test.skip('Developermail is defunct');
     const mailbox = new IntegrationMailbox("DEVELOPER")
 
     let emailAddress: string;
